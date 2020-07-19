1421943cd2124172dbef134e18b29ffa2cd46587
suraj9716 <ss971600@gmail.com>
2020-07-19 15:19:54 +0530

    Updates

---
+++ precache-manifest.1a58e8605198944a1edfed01c40fedfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db1e29b7130af42cb28e0b3c8b60edc4",
+    "url": "/devik2.git/index.html"
+  },
+  {
+    "revision": "0c85726c9b5f54db8558",
+    "url": "/devik2.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "917a9b45cc95ae97b29e",
+    "url": "/devik2.git/static/js/2.b55125a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/devik2.git/static/js/2.b55125a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c85726c9b5f54db8558",
+    "url": "/devik2.git/static/js/main.3c8f5545.chunk.js"
+  },
+  {
+    "revision": "bfd1075a1477a7662314",
+    "url": "/devik2.git/static/js/runtime-main.56907f39.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/devik2.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
